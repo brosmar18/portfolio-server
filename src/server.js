f11624abd21da848b527d443b6809648e0d87f84
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-22 12:28:08 -0500

    server working in specified port

--- src/server.js
+++ src/server.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.status(200).send('Hello World');
+});
+
+const start = () => {
+    app.listen(PORT, () => console.log('server is running'));
+};
+
+module.exports = { start, app };
+
