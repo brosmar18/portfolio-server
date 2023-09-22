9c75cec1dae030d7bb3ef0a4eecb60d7d4bea5ca
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-22 12:34:51 -0500

    projects route added

--- src/server.js
+++ src/server.js
@@ -10,6 +10,12 @@ app.get('/', logger, (req, res, next) => {
     res.status(200).send(req.log);
 });
 
+app.get('/projects', (req, res, next) => {
+    next('Project route');
+});
+
+
+
 const start = () => {
     app.listen(PORT, () => console.log('server is running'));
 };
