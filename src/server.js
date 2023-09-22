063f4145f09639be4e02a08ca86bd2f41ea6d58b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-22 12:37:54 -0500

    notFound 404 error handler is working

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const logger = require('./middleware/logger.js');
+const notFound = require('./error-handlers/404.js');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -14,7 +15,7 @@ app.get('/projects', (req, res, next) => {
     next('Project route');
 });
 
-
+app.get('*', notFound);
 
 const start = () => {
     app.listen(PORT, () => console.log('server is running'));
